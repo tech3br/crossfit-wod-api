49ad09305ec53e20d40b63761f4a6e0354ea59b6
tech3br <dfsdireito95@gmail.com>
2022-05-07 01:32:36 -0300

    feat(routes): add first routes folder and import this

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
+const v1Router = require("./v1/routes");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h2>It's Working!</h2>");
-});
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
