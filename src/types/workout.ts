41afc0c869429aa3e8fc8ce2dbd09abe225f93a2
tech3br <dfsdireito95@gmail.com>
2022-05-10 00:46:17 -0300

    feat(types): add workoutprops types

---
+++ src/types/workout.ts
@@ -0,0 +1,10 @@
+export interface WorkoutProps {
+  id: string;
+  name: string;
+  mode: string;
+  equipment: string[];
+  exercises: string[];
+  createdAt: string;
+  updatedAt: string;
+  trainerTips: string[];
+}
